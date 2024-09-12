c42bbf103e4ba73278273300d4c1b3693b74e34f
trshkv <gonzjv@gmail.com>
2024-09-12 16:08:18 +0300

    package: tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import { Outlet } from 'react-router'
 
 import './App.css'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
+    <Header/>
     <Outlet/>
+    <Footer/>
    </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="border-1 border-b-yellow-300">
+            footer
+        </footer>
+    )
+}
+
+export default Footer;
