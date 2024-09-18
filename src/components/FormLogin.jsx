02f2c5cc71599d3d4ae4225e8b845ca524e66aaa
gonzjv <gonzjv@ya.ru>
2024-09-18 12:51:52 +0300

    fix: user login

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -14,14 +14,15 @@ function FormLogin() {
     async function loginUser(reqJson) {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-        
+        console.log("reqJson", reqJson);
+
         const options = {
             headers: myHeaders,
             method : "POST",
             body: reqJson
         };
         
-        const res = await fetch("http://localhost:8080/user/login", options);
+        const res = await fetch("http://localhost:3000/api/v1/user/login", options);
         const json = await res.json();
         console.log(json); 
     
@@ -34,8 +35,8 @@ function FormLogin() {
     return (
         <form className="p-10 flex flex-col items-end gap-8 shadow-xl rounded-lg shadow-yellow-200 bg-slate-900">
             <div className="flex justify-center items-center gap-3">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
                 <input
                 className="h-14 border-2 rounded-md px-6"
@@ -46,8 +47,8 @@ function FormLogin() {
                     }} type="text" name="name" placeholder="email..."/>
             </div>
             <div className="flex justify-center items-center gap-3">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                 </svg>
                 <input
                 className="h-14 border-2 rounded-md px-6"
@@ -56,7 +57,10 @@ function FormLogin() {
                     
                     }} type="text" name="name" placeholder="password..."/>
             </div>
-            <button onClick={() => handleBtnClick()}
+            <button onClick={(e) => {
+                e.preventDefault();
+                handleBtnClick();
+            }}
                     className="flex justify-center items-center 
                     w-[15vw] h-[7vh] rounded-full text-slate-600 
                     border-2 border-slate-200
